7e5abd0c170eea15450bbf53b4a6643f11bc3a12
paul-popa <paul.sebastian19@yahoo.com>
2022-07-03 18:07:38 +0300

    add react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./Home";
+import Test from "./Test";
 
 function App() {
   return (
     <div className="App">
-      Test
+      <BrowserRouter>
+        <Routes>
+          <Route path="/ghp-project" element={<Home />}></Route>
+          <Route path="/ghp-project/test" element={<Test />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
