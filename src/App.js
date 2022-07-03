7a6698676a3ba680b7e8487bc9bf0b0a193c6119
paul-popa <paul.sebastian19@yahoo.com>
2022-07-03 18:33:54 +0300

    fix App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Home";
 import Test from "./Test";
@@ -7,10 +7,11 @@ import Test from "./Test";
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <HashRouter><Routes>
         <Route path="/ghp-project" element={<Home />}></Route>
         <Route path="/ghp-project/test" element={<Test />}></Route>
-      </Routes>
+      </Routes></HashRouter>
+      
     </div>
   );
 }
