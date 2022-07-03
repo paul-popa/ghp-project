f90ab9d2bfd7c7fe4f329d29e2ac0ff7785447cf
paul-popa <paul.sebastian19@yahoo.com>
2022-07-03 19:54:31 +0300

    update again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-
 import Home from "./Home";
 import Test from "./Test";
 
@@ -8,7 +7,7 @@ function App() {
   return (
     <div className="App">
         <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/ghp-project" element={<Home />}></Route>
         <Route path="/test" element={<Test />}></Route>
         <Route path="*" element={<Home />}></Route>
       </Routes> 
