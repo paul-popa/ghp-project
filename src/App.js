61dfdf4d7a66c376a72fb56f37ed84d5a37deb60
paul-popa <paul.sebastian19@yahoo.com>
2022-07-03 18:12:15 +0300

    update react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./Home";
 import Test from "./Test";
@@ -7,12 +7,10 @@ import Test from "./Test";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/ghp-project" element={<Home />}></Route>
-          <Route path="/ghp-project/test" element={<Test />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/ghp-project" element={<Home />}></Route>
+        <Route path="/ghp-project/test" element={<Test />}></Route>
+      </Routes>
     </div>
   );
 }
